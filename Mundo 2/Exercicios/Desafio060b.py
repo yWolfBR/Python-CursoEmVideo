db370c745f55adc0a5a96ddc27ab4d2f4001165c
yWolfBR <71306144+yWolfBR@users.noreply.github.com>
2021-06-22 19:24:24 -0300

    Fixed 060b

--- Mundo 2/Exercicios/Desafio060b.py
+++ Mundo 2/Exercicios/Desafio060b.py
@@ -1,7 +1,7 @@
 n = int(input('Digite um número para saber seu Fatorial: '))
 print('Calculando {}! = '.format(n), end='')
-for c in range(n, 0, -1):
+for c in range(n, 1, -1):
     print('{}'.format(c), end='')
-    print(' * ' if c > 1 else ' = ', end='')
+    print(' * ' if c > 2 else ' * 1 = ', end='')
     n = n * (c - 1)
 print('{}'.format(n))
